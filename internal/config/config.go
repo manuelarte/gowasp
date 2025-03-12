5a896d33730eb88b9204818853270028469a1f1c
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-03-12 14:52:13 +0100

    Improve dockerfile (#8)
    * improving Dockerfile
    
    * adding version
    
    * improve Dockerfile
    
    ---------
    
    Co-authored-by: manuelarte <manuel.doncel.martos@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,5 +2,5 @@ package config
 
 type Config struct {
 	MigrationSourceURL string `env:"MIGRATION_SOURCE_URL" envDefault:"file://./resources/migrations"`
-	TemplatesPath      string `env:"TEMPLATES_PATH" envDefault:"web/templates/**/*"`
+	WebPath            string `env:"WEB_PATH" envDefault:"web"`
 }
