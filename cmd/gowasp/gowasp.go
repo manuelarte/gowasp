0a1ef8ae5e21bed44a3d8c3470a610a9b55c09f1
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-04-15 04:07:45 +0200

    no panic and use revive (#10)
    * no panic and use revive

--- cmd/gowasp/gowasp.go
+++ cmd/gowasp/gowasp.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
+	"log/slog"
+	"os"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/gin-contrib/cors"
@@ -22,15 +24,13 @@ import (
 	"github.com/manuelarte/gowasp/internal/services"
 )
 
-func renderUnsafe(s string) template.HTML {
-	//#nosec G203
-	return template.HTML(s)
-}
-
 func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
 	cfg, err := env.ParseAs[config.Config]()
 	if err != nil {
-		panic(err)
+		logger.Error("error parsing the configuration", "error", err)
+		return
 	}
 
 	db, err := config.MigrateDatabase(cfg.MigrationSourceURL)
@@ -87,6 +87,12 @@ func main() {
 
 	err = r.Run()
 	if err != nil {
-		panic(err)
+		logger.Error("error running the application", "error", err)
+		return
 	}
+}
+
+func renderUnsafe(s string) template.HTML {
+	//#nosec G203
+	return template.HTML(s)
 }
