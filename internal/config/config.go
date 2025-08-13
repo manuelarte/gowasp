e37b40687045fc80690e665f9ac801ac6f72c3d1
manuelarte <manueldoncelmartos@gmail.com>
2025-08-13 20:01:15 +0200

    refactor: embedding migration files

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 type Config struct {
-	Address            string `env:"ADDRESS" envDefault:":8083"`
-	MigrationSourceURL string `env:"MIGRATION_SOURCE_URL" envDefault:"file://./resources/migrations"`
-	WebPath            string `env:"WEB_PATH" envDefault:"web"`
+	Address string `env:"ADDRESS" envDefault:":8083"`
+	WebPath string `env:"WEB_PATH" envDefault:"web"`
 }
--- internal/config/migrate.go
+++ internal/config/migrate.go
@@ -2,14 +2,17 @@ package config
 
 import (
 	"database/sql"
+	"embed"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
-func MigrateDatabase(migrationSourceURL string) (*sql.DB, error) {
+func MigrateDatabase(fs embed.FS) (*sql.DB, error) {
 	maxOpenConnections := 3
 	connMaxxTime := time.Hour
 	db, err := sql.Open("sqlite3", "file:test.db?cache=shared&mode=memory")
@@ -23,17 +26,21 @@ func MigrateDatabase(migrationSourceURL string) (*sql.DB, error) {
 
 	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error creating db driver: %w", err)
 	}
-	m, err := migrate.NewWithDatabaseInstance(
-		migrationSourceURL,
+
+	d, err := iofs.New(fs, "resources/migrations")
+	if err != nil {
+		return nil, fmt.Errorf("error getting the migration files: %w", err)
+	}
+	m, err := migrate.NewWithInstance("iofs", d,
 		"test", driver)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error creating the migration instance: %w", err)
 	}
 
 	if err = m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		return nil, err
+		return nil, fmt.Errorf("error migrating database: %w", err)
 	}
 
 	return db, nil
