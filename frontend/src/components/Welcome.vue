ffdaae7efe55fc767608a6679496403140491e12
manuelarte <manueldoncelmartos@gmail.com>
2025-09-11 22:15:42 +0200

    wip welcome page

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -6,10 +6,16 @@
   <h1>Welcome gowasp to Gowasp website</h1>
 
   <v-alert
+    density="compact"
     text="Warning: This is just for information purposes: Password hash: cf0e7120ef18914d3c4a2f27fa236956"
     title="Info"
     type="warning"
+    variant="outlined"
   />
+
+  <br>
+
+  <h2>Posts</h2>
 </template>
 
 <style scoped lang="sass">
