59db9838fac374c7c342f26996e8d9d7541c07d5
manuelarte <manueldoncelmartos@gmail.com>
2025-09-12 16:26:56 +0200

    wip posts

--- frontend/src/pages/posts/[id].vue
+++ frontend/src/pages/posts/[id].vue
@@ -28,12 +28,12 @@
 <template>
   <div class="card">
     <v-skeleton-loader
-      v-if="isLoadingComments"
+      v-if="isLoadingPost"
       class="mx-auto border"
       max-width="300"
       type="image, article"
     />
-    <v-card v-else-if="errorComments" color="red-lighten-2" variant="tonal">Error</v-card>
+    <v-card v-else-if="errorPost" color="red-lighten-2" variant="tonal">Error</v-card>
     <v-card v-else class="card">
       <v-card-title>{{ post.title }}</v-card-title>
       <v-card-text>{{ post.content }}</v-card-text>
@@ -41,30 +41,33 @@
 
     <br>
 
-    <p>This post has {{ postCommentsPageAndUsers.commentsPage?.data.length }} comment(s)</p>
+    <v-skeleton-loader v-if="isLoadingComments" type="card"></v-skeleton-loader>
+    <template v-else>
+      <p>This post has {{ postCommentsPageAndUsers.commentsPage?.data.length }} comment(s)</p>
 
-    <v-card
-      v-for="comment in postCommentsPageAndUsers.commentsPage?.data ?? []"
-      :key="comment.id"
-      class="card"
-      :subtitle="comment.postedAt"
-    >
-      <template #prepend>
-        <v-avatar
-          color="brown"
-          size="large"
-        >
-          <span class="text-h5">{{ getUsername(comment.userId).substring(0, 2).toUpperCase() }}</span>
-        </v-avatar>
-      </template>
-      <template #title>
-        <span class="font-weight-black">{{ getUsername(comment.userId) }}</span>
-      </template>
+      <v-card
+        v-for="comment in postCommentsPageAndUsers.commentsPage?.data ?? []"
+        :key="comment.id"
+        class="card"
+        :subtitle="comment.postedAt"
+      >
+        <template #prepend>
+          <v-avatar
+            color="brown"
+            size="large"
+          >
+            <span class="text-h5">{{ getUsername(comment.userId).substring(0, 2).toUpperCase() }}</span>
+          </v-avatar>
+        </template>
+        <template #title>
+          <span class="font-weight-black">{{ getUsername(comment.userId) }}</span>
+        </template>
 
-      <v-card-text class="pt-4">
-        {{ comment.comment }}
-      </v-card-text>
-    </v-card>
+        <v-card-text class="pt-4">
+          {{ comment.comment }}
+        </v-card-text>
+      </v-card>
+    </template>
   </div>
 </template>
 
