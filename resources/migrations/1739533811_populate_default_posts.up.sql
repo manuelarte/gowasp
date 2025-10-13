ab484ef7de4c4638484bf15e98b1ea0f55c25944
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-10-13 11:28:54 +0200

    fix: change favicon, and post content (#64)

--- resources/migrations/1739533811_populate_default_posts.up.sql
+++ resources/migrations/1739533811_populate_default_posts.up.sql
@@ -1,7 +1,7 @@
 INSERT INTO posts (id, created_at, updated_at, posted_at, user_id, title, content)
 VALUES
     (1, current_timestamp, current_timestamp, '2025-01-01 12:00:00', 2, "1st Post", "This is my 1st post"),
-    (2, current_timestamp, current_timestamp, '2025-02-01 12:00:00', 2, "2nd Post", "This is my 2nd post"),
-    (3, current_timestamp, current_timestamp, '2025-03-01 12:00:00', 2, "3rd Post", "This is my 3rd post"),
-    (4, current_timestamp, current_timestamp, '2025-04-01 12:00:00', 2, "4th Post", "This is my 4th post"),
-    (5, current_timestamp, current_timestamp, '2025-05-01 12:00:00', 2, "5th Post", "This is my 5th post")
+    (2, current_timestamp, current_timestamp, '2025-02-01 12:00:00', 2, "It's called football, not soccer", "Stop calling it soccer."),
+    (3, current_timestamp, current_timestamp, '2025-03-01 12:00:00', 2, "Getting older sucks", "I can tell you, getting older is not nice."),
+    (4, current_timestamp, current_timestamp, '2025-04-01 12:00:00', 2, "Cats are evil", "Ok, let's look at the evidence. Cats are evil, no doubts."),
+    (5, current_timestamp, current_timestamp, '2025-05-01 12:00:00', 2, "Cats are nice", "Ok, let's look at the evidence. Cats are nice, no doubts.")
